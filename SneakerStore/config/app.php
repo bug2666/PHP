6c3640d4756de8fb11bfd02f528c6973d65471de
267T <vanloa402@gmail.com>
2026-04-20 08:51:24 +0700

    backup1

--- SneakerStore/config/app.php
+++ SneakerStore/config/app.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_URL', 'https://proxyio.online');
+define('APP_URL', 'https://proxyio.online/SneakerStore');
 /* define('APP_URL', 'http://localhost/PHP/SneakerStore'); */
 ?>
 <!-- define('APP_URL', 'https://proxyio.online'); -->
--- SneakerStore/includes/menu.php
+++ SneakerStore/includes/menu.php
@@ -1,4 +1,4 @@
-<?php require_once __DIR__ . '/config/app.php'; ?>
+<?php require_once __DIR__ . '/../config/app.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
